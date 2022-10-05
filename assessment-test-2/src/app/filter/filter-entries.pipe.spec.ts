0f0ad46971fa1484adbf89be7c52bd0ac3249b69
Maria Barbosa <mbarbosa@architech.ca>
2022-10-05 09:32:02 -0400

    Removing front end filtering

--- assessment-test-2/src/app/filter/filter-entries.pipe.spec.ts
+++
@@ -1,8 +0,0 @@
-import { FilterEntriesPipe } from './filter-entries.pipe';
-
-describe('FilterEntriesPipe', () => {
-  it('create an instance', () => {
-    const pipe = new FilterEntriesPipe();
-    expect(pipe).toBeTruthy();
-  });
-});
